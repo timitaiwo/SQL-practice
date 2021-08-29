38f32b8cc8cbe5ed5c6e2c88d49d403c2b244ec0
Timi_Tim <31576028+timitaiwo@users.noreply.github.com>
2021-08-29 15:53:58 +0100

    Assignment2 - Practical SQL

---
+++ Assignment_2.sql
@@ -0,0 +1,18 @@
+/* 1. Write a query that lists the schools in alphabetical order along 
+with teachers ordered by last name A–Z */
+SELECT school, last_name, first_name
+FROM teachers
+ORDER BY school, last_name;
+
+/* 2. Write a query that finds the one teacher whose first name starts with the 
+letter S and who earns more than $40,000 */
+SELECT *
+FROM teachers
+WHERE (first_name LIKE ('s%') OR first_name LIKE ('S%')) AND salary > 40000;
+
+/* 3. Rank teachers hired since January 1, 2010, ordered by highest paid to 
+lowest */
+SELECT *
+FROM teachers
+WHERE hire_date > '2010-01-01'
+ORDER BY salary DESC;
